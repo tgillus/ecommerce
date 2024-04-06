b888865fc11ecd5fe4277c637eca03ae04520d48
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2024-04-06 19:21:28 -0400

    refactor: convert to arrow functions

--- lib/vendor/pkg/package.ts
+++ lib/vendor/pkg/package.ts
@@ -4,15 +4,13 @@ import { Assert } from '../type/assert.js';
 export class Package {
   constructor(private readonly assert: Assert) {}
 
-  rootDir() {
+  rootDir = () => {
     const pkgDir = packageDirectorySync();
 
     this.assert.string(pkgDir);
 
     return pkgDir;
-  }
+  };
 
-  static build() {
-    return new Package(new Assert());
-  }
+  static build = () => new Package(new Assert());
 }
