faf430712cebeec4353e5c0e208b984bfbe96bcc
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2024-04-16 22:29:57 -0400

    chore: upgrade dependencies

--- lib/lambda/products/application/operation/validation/create-validator.ts
+++ lib/lambda/products/application/operation/validation/create-validator.ts
@@ -1,6 +1,6 @@
 import { formatErrorSync } from '@effect/schema/ArrayFormatter';
 import * as S from '@effect/schema/Schema';
-import { Effect, Layer, pipe } from 'effect';
+import { Effect, Inspectable, Layer, Logger, pipe } from 'effect';
 import { SafeJson } from '../../../../../vendor/type/safe-json.js';
 import { ValidationError } from '../../../../common/application/error/validation-error.js';
 import { RequestParams } from '../../../../common/request/request-params.js';
@@ -23,6 +23,22 @@ export const CreateValidatorLive = Layer.succeed(Validator, {
       }))
     ),
 });
+
+export const logger = Logger.make(({ logLevel, message }) => {
+  globalThis.console.log(
+    Inspectable.stringifyCircular({
+      foo: 'bar',
+      logLevel: logLevel.label,
+      message,
+      timestamp: new Date().toISOString(),
+    })
+  );
+});
+const layer = Logger.replace(Logger.defaultLogger, logger);
+
+const program = Effect.log('Hello, World!');
+// Effect.runSync(Effect.provide(program, Logger.json));
+Effect.runSync(Effect.provide(program, layer));
 
 export const ProductSchema = S.Struct({
   description: S.String,
