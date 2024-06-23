c91fc5acb4cff686ff095f64e1acb11095cfa657
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2024-06-23 18:44:15 -0400

    refactor: clean up

--- test/e2e/products/products.test.ts
+++ test/e2e/products/products.test.ts
@@ -1,22 +1,22 @@
-import { faker } from '@faker-js/faker';
-import { request, spec } from 'pactum';
-import { describe, test } from 'vitest';
+import { faker } from "@faker-js/faker";
+import { request, spec } from "pactum";
+import { describe, test } from "vitest";
 
 request.setBaseUrl(
-  `https://${process.env.AWS_API_GATEWAY_ID}.execute-api.${process.env.AWS_API_GATEWAY_REGION}.amazonaws.com/prod`
+	`https://${process.env.AWS_API_GATEWAY_ID}.execute-api.${process.env.AWS_API_GATEWAY_REGION}.amazonaws.com/prod`,
 );
 
-describe('POST /products', () => {
-  test('saves new products', async () => {
-    await spec()
-      .post('/products')
-      .withBody({
-        description: faker.commerce.productDescription(),
-        name: faker.commerce.productName(),
-        price: faker.commerce.price({
-          dec: 2,
-        }),
-      })
-      .expectStatus(200);
-  });
+describe("POST /products", () => {
+	test("saves new products", async () => {
+		await spec()
+			.post("/products")
+			.withBody({
+				description: faker.commerce.productDescription(),
+				name: faker.commerce.productName(),
+				price: faker.commerce.price({
+					dec: 2,
+				}),
+			})
+			.expectStatus(200);
+	});
 });
