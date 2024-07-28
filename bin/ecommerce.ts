6f491dea95a0edb982cbe4eb813a3f8a766b5d1f
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2024-07-27 20:42:12 -0400

    feat: add cognito auth

--- bin/ecommerce.ts
+++ bin/ecommerce.ts
@@ -2,6 +2,7 @@
 import * as cdk from 'aws-cdk-lib';
 import 'source-map-support/register';
 import { ApiStack } from '../lib/api/api-stack.js';
+import { AuthStack } from '../lib/auth/auth-stack.js';
 import { DynamoStack } from '../lib/dynamo/dynamo-stack.js';
 
 const app = new cdk.App();
@@ -17,4 +18,8 @@ const { productsTable } = new DynamoStack(app, 'EcommerceDynamoStack', {
 new ApiStack(app, 'EcommerceApiStack', {
   env,
   productsTable,
+});
+
+new AuthStack(app, 'EcommerceAuthStack', {
+  env,
 });
