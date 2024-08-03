e25dc30d98adb07cfc08a1fba5706849b26117c7
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2024-08-03 18:17:41 -0400

    chore: remove commented out code

--- lib/lambda/products/domain/model/product.ts
+++ lib/lambda/products/domain/model/product.ts
@@ -1,11 +1,5 @@
 import * as S from '@effect/schema/Schema';
 
-// export interface Product {
-//   readonly description: string;
-//   readonly name: string;
-//   readonly price: string;
-// }
-
 export type Product = S.Schema.Type<typeof CreateProductSchema>;
 
 export const CreateProductSchema = S.Struct({
